f7c0ca3be55fa798557de426f0681ea52786e9c9
naveen kumar <nk1436350@gmail.com>
2025-09-06 16:32:45 +0530

    create a Home screen

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import AppNavigation from './src/appNavigation/AppNavigation'
+const App = () => {
+  return (
+   <AppNavigation />
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
---
+++ src/appNavigation/AppNavigation.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Home from '../screen/Home';  
+const Stack = createNativeStackNavigator();
+
+const AppNavigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        {/* <Stack.Screen name="Details" component={DetailsScreen} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default AppNavigation
+
+const styles = StyleSheet.create({})        
+
+
